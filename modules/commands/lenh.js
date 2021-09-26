ec3e2c2ef82a7ba062cd7a6a306ea4925cf93222
vanhuy15012006 <ngvanhuy1501@gmail.com>
2021-09-26 19:09:58 +0700

    first commit

---
+++ modules/commands/lenh.js
@@ -0,0 +1,11 @@
+﻿module.exports.config = {
+	name: "ad",
+	version: "1.0.0",
+	hasPermssion: 0,
+	credits: "Nguyễn Văn Huy",
+	description: "lệnh hay sử dụng",
+	commandCategory: "Lệnh Bot Hay sử dụng",
+	cooldowns: 0
+};
+
+module.exports.run = ({ event, api }) => api.sendMessage(`\n!Lệnh Thường sử dụng!\nHelp,Say,Boxfamily,Sim,locmem,ad,donate,kiss,Hug,Setname,uid,...\nChúc các bạn sử dụng bot vui vẻ===`, event.threadID, event.messageID);
